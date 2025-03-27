3dd9cb954af0e556db8770c9d838ef95208fb39a
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-03-27 16:01:30 +0600

    .

--- models/Event.js
+++ models/Event.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
-  eventDate: { type: Number, required: true },
+  eventDate: { type: String, required: true },
 });
 
 export default mongoose.models.Event || mongoose.model("Event", eventSchema);
