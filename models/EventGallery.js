6d0374c46a05fef7c5ba0c7eaf414bb8497f689e
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-03-28 17:41:21 +0600

    .

--- models/EventGallery.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const EventSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-});
-
-export default mongoose.model("EventGallery", EventSchema);
