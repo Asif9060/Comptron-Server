f204d4aaa00190884ab3bbbe7b7476f436c96a76
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-03-29 15:56:23 +0600

    .

--- models/DeletedMember.js
+++ models/DeletedMember.js
@@ -7,7 +7,7 @@ const DeletedMemberSchema = new mongoose.Schema({
     bio: String,
     socials: Object,
     image: String,
-    deletedAt: { type: Date, default: Date.now } // Store deletion time
+    deletedAt: { type: Date, default: Date.now + 6 * 60 * 60 * 1000 } // Store deletion time
 });
 
 const DeletedMember = mongoose.model("DeletedMember", DeletedMemberSchema);
