6608ba30a0cc4b1f68c974452f48e0c1a8c147aa
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-04-14 21:39:01 +0600

    .

--- models/TempUser.js
+++ models/TempUser.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const tempUserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    password: { type: String, required: true },
+    phone: { type: String, required: false },
+    password: { type: String, required: false },
     otp: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, expires: 600 },
     
