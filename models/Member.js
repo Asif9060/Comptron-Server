9fe13ddb61ebb79f34389659883ec2642b2d701e
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-04-18 16:00:51 +0600

    .

--- models/Member.js
+++ models/Member.js
@@ -7,14 +7,29 @@ const memberSchema = new mongoose.Schema({
     unique: true,
   },
   name: String,
+  email: String,
+  phone: String,
+  skills: String,
   role: String,
   //email: String,
   image: String, // URL or base64
   // bio: String,
   socials: {
     github: String,
-    linkedin: String
-  }
+    linkedin: String,
+    portfolio: String,
+    cv: String,
+  },
+  validityDate: { 
+    type: Date, 
+    required: true, 
+    default: () => {
+      const today = new Date();
+      today.setFullYear(today.getFullYear() + 1); // Add 1 year
+      return today;
+    }
+  },
+  isValid: Boolean,
 });
 
 // Export the model as a default export
