ca777d62eb3775aefb64bf77e517eb5dc1a5404d
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-04-24 03:47:34 +0600

    .

--- models/DetailedEvent.js
+++ models/DetailedEvent.js
@@ -5,11 +5,8 @@ const eventSchema = new mongoose.Schema({
   description: { type: String, required: true },
   mainImage: { type: String, required: false }, // URL of main image
   galleryImages: { type: [String], required: true },
-  date: { type: String, required: true },        // e.g., "2025-04-20"
-  time: { type: String, required: true },        // e.g., "14:30"
-  dateTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },  
-   
+  startDateTime: { type: Date, required: true },
+  endDateTime: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
