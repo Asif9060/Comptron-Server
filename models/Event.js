85c6981185bcad34aaf577c927c23bbd2efa8227
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-04-25 01:41:05 +0600

    .

--- models/Event.js
+++ models/Event.js
@@ -1,15 +1,8 @@
 import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  date: { type: String, required: true },
-  time: { type: String, required: true },
-  durationDays: { type: Number, default: 1 },
-  dateTime: { type: Date, required: true },
-  endTime: { type: Date, required: true },
-  mainImage: { type: String },
-  galleryImages: { type: [String], default: [] },
+  name: { type: String, required: true },
+  eventDate: { type: String, required: true },
 });
 
 export default mongoose.models.Event || mongoose.model("Event", eventSchema);
