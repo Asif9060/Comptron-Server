ad6302141fb5a5424ab0926c560f7f614a9b1861
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-05-15 18:23:19 +0600

    .

---
+++ models/PendingUser.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const pendingUserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    firebaseUserId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    studentId: {
+      type: String,
+      default: "",
+    },
+    bloodGroup: {
+      type: String,
+      default: "",
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const PendingUser = mongoose.model("PendingUser", pendingUserSchema);
+
+export default PendingUser; 
